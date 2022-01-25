1f9b2c9b5edaa7dc0ce67b17bde78f7c85a8ae1b
Ed Halliwell <edhalliwell@hotmail.com>
2022-01-25 11:11:57 +0000

    Add meta data for columns endpoint

---
+++ controllers/getColumnMetaDataForABoard.ts
@@ -0,0 +1,40 @@
+import { client } from "../src/server";
+import type { Request, Response } from "express";
+
+export const getColumnMetaDataForABoard = async (
+  req: Request,
+  res: Response
+) => {
+  const board_id = parseInt(req.params.board_id);
+
+  // check board exists
+  const getBoardById = await client.query(
+    "SELECT * FROM boards where id = $1",
+    [board_id]
+  );
+
+  if (getBoardById.rows.length > 0) {
+    try {
+      const dbRes = await client.query(
+        "SELECT id as column_ids FROM columns WHERE board_id = $1",
+        [board_id]
+      );
+      if (dbRes.rows.length > 0) {
+        res.status(200).json({
+          message: "Successfully retrieved metadata for columns",
+          data: dbRes.rows,
+        });
+      } else {
+        res.status(500).json({
+          message: "Something went wrong",
+        });
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  } else {
+    res.status(404).json({
+      message: "No board with this id found",
+    });
+  }
+};
